35498181bf4a5ed4b94ac1fc2003df4818944b32
SahayaRinisha <sahayarinisha23@gmail.com>
2024-07-12 08:55:15 -0700

    Add files via upload

---
+++ task03.py
@@ -0,0 +1,35 @@
+import re
+
+#Passoword must be at least 8 character long
+
+password= input("Enter Your Password: ")
+
+if len(password) < 8:
+
+   print("Password must be at least 8 charcters long.")
+
+#Password must contain at least one uppercase letter.
+
+elif not re.search("[A-Z]", password):
+
+
+
+    print("Password must contain at least one uppercase letter.")
+
+#Password must contain at least one lowercase letter.
+
+elif not re.search("[a-z]", password):
+
+    print("Password must contain at least one lowercase letter.")
+
+#Password must contain at least one number.
+
+
+
+elif not re.search("[0-9]",password):
+
+    print("Password must contain at least one number.")
+
+else:
+
+    print("Password is strong")
